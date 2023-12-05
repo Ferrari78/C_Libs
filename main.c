9d0b11f51bca9bb7d405c6f6afb8f4511defaa7b
Ferrari78 <88674950+Ferrari78@users.noreply.github.com>
2023-12-05 12:39:22 +0100

    update

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include "module/LinkedList.h"
 
 int main() {
-    printf("Hello, World!\n");
-    List* list = create_List();
+    List *list = create_List();
     char b[255] = "hallo";
     int a = 23;
-    add(list,  b,STRING);
-    add(list,  a, INT);
-    printf("position %d\n", searchData(list,b,STRING));
+    add(list, b, STRING);
+    add(list, a, INT);
+    printf("position %d\n", searchData(list, b, STRING));
     printNodeData(list->end);
+
     return 0;
 }
